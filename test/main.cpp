3cb6f3b836c168f90dbfd595c54ac9b8d9e1716a
Meraj <meraj.sheikhhaa@gmail.com>
2021-03-22 12:45:33 -0700

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "../SiboSql/include/SiboSql.h"
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    int i;
+    for(i = 0; i<argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
 int main() {
     std::string dbName = "testdb.db";
+    // Create Database
     SiboSql siboSql = SiboSql(dbName);
     siboSql.table("persons")
     .id()
@@ -14,4 +23,9 @@ int main() {
     .column("name","VARCHAR")
     .column("user_id","INTEGER")
     .init();
+    // End Create Database
+    sqlite3_stmt* stmt ;
+    siboSql.query("INSERT INTO persons (first_name,last_name) VALUES ('jafar','jafarian')"); // insert data
+    stmt = siboSql.query("SELECT * FROM persons"); // select data
+    siboSql.query("SELECT * FROM persons",callback); // with callback function
 }
