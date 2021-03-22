036bba318a3a783d5fe124ad648a9a370644d063
Meraj <meraj.sheikhhaa@gmail.com>
2021-03-22 03:47:50 -0700

    update

--- include/SiboSql.h
+++ include/SiboSql.h
@@ -7,8 +7,25 @@
 
 #include <iostream>
 #include <sqlite3.h>
+#include <vector>
 class SiboSql {
-
+private:
+    sqlite3 *db;
+    int rc;
+    sqlite3_stmt stmt;
+    std::string tableQuery;
+    std::vector<std::string> queries;
+public:
+    SiboSql();
+    SiboSql(char* dbName);
+    SiboSql(std::string dbName);
+    SiboSql setVersion(int version);
+    SiboSql table(std::string tableName);
+    SiboSql id();
+    SiboSql column(std::string columnName,std::string dataType);
+    void init();
+    sqlite3_stmt* query(std::string query);
+    void query(std::string query,std::function<int (void*,int,char**,char**)> callback);
 };
 
 
--- src/SiboSql.cpp
+++ src/SiboSql.cpp
@@ -2,4 +2,70 @@
 // Created by Meraj on 3/22/2021.
 //
 
+#include <functional>
 #include "../include/SiboSql.h"
+
+SiboSql::SiboSql() {
+
+}
+
+SiboSql::SiboSql(char* dbName) {
+    rc = sqlite3_open(dbName, &db);
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+}
+
+SiboSql::SiboSql(std::string dbName) {
+    rc = sqlite3_open(dbName.c_str(), &db);
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+}
+
+SiboSql SiboSql::column(std::string columnName, std::string dataType) {
+    this->tableQuery += columnName + " " + dataType+",";
+    return *this;
+}
+
+SiboSql SiboSql::table(std::string tableName) {
+    if (this->tableQuery.empty()){
+        this->tableQuery += "CREATE TABLE IF NOT EXISTS " + tableName + "(";
+    }else{
+        this->tableQuery.erase(std::prev(this->tableQuery.end()));
+        this->tableQuery += ");";
+        this->queries.push_back(this->tableQuery);
+    }
+    return *this;
+}
+
+void SiboSql::init() {
+    this->tableQuery.erase(std::prev(this->tableQuery.end()));
+    this->tableQuery += ");";
+    this->queries.push_back(this->tableQuery);
+
+    for(std::string query:this->queries){
+        sqlite3_prepare(db,query.c_str(),-1,stmt);
+        sqlite3_step(stmt);
+    }
+}
+
+SiboSql SiboSql::id() {
+    this->tableQuery += "id INTEGER PRIMARY KEY AUTOINCREMENT,";
+    return *this;
+}
+
+sqlite3_stmt* SiboSql::query(std::string query) {
+    sqlite3_prepare(db,query.c_str(),-1,stmt);
+    sqlite3_step(stmt);
+    return stmt;
+}
+
+void SiboSql::query(std::string query, std::function<int (void*,int,char**,char**)> callback) {
+sqlite3_exec(db, query.c_str(), NULL, 0, NULL);
+
+}
+
+
+
+
